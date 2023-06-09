99aedda4d28a7a0ce74d4257ad6e9b895aeae4b1
isaacg <iwanyoike65@gmail.com>
2023-06-09 17:53:24 +0300

    Add update counter functions

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,15 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  after_save :update_post_counter
+
   def recent_comments
     comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+
+  def update_post_counter
+    author.update(post_counter: author.posts.count)
   end
 end
