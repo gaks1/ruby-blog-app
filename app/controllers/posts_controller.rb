06c7b2e3aa5dd636c26f231e1e8935a227ba8a1a
isaacg <iwanyoike65@gmail.com>
2023-06-16 20:38:53 +0300

    Add into actions listing of all users and posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,11 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @user = User.find_by_id(params[:user_id])
+    @posts = @user.posts
+  end
 
-  def show; end
+  def show 
+    @post = Post.find_by_id(params[:id])
+    @comments = @post.comments
+  end
 end
