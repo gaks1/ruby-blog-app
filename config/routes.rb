338c627c2a78709bd321cf64084926382fa05129
isaacg <iwanyoike65@gmail.com>
2023-06-17 14:50:16 +0300

    Add new comment

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
    root "users#index"
-   resources :users, only: [:show] do
+   resources :users, only: [:index, :show] do
     resources :posts, only: [:new, :create, :index, :show] do
       resources :comments, only: [:new, :create]
       resources :likes, only: [:create]
