960171e9d05e9659876716823b80a2c9a9e2a56f
isaacg <iwanyoike65@gmail.com>
2023-06-17 14:27:35 +0300

    Add new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,27 @@ class PostsController < ApplicationController
     @post = Post.find_by_id(params[:id])
     @comments = @post.comments
   end
+
+  def new
+    @user = current_user
+    @post = Post.new
+  end
+
+  def create
+    @user = current_user
+    @post = @user.posts.build(post_param)
+    @post.comments_counter = 0
+    @post.likes_counter = 0
+    if @post.save
+      redirect_to user_post_path(current_user, @post)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_param
+    params.require(:post).permit(:title, :text)
+  end
 end
