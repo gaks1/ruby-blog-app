86ab4168603a50134da3829728efff22c152f3a6
isaacg <iwanyoike65@gmail.com>
2023-06-17 15:17:14 +0300

    Add new likes

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,20 @@
+class LikesController < ApplicationController
+    def new
+        @like = Like.new
+    end
+
+   def create
+            @post = Post.find(params[:post_id])
+            @like = @post.likes.new
+            @like.post_id = @post.id
+            @like.author_id = current_user.id
+        
+            if @like.save
+              redirect_to user_post_path(current_user, @post)
+            else
+              render.new
+            end
+        end
+
+
+  end
