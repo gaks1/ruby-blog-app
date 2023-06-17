3971238841994a5e03bb6d145ce115e4d443f1d4
isaacg <iwanyoike65@gmail.com>
2023-06-17 15:18:07 +0300

    Fix linter errors

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,24 @@
 class CommentsController < ApplicationController
-    def new
-        @comment = Comment.new
-    end
+  def new
+    @comment = Comment.new
+  end
 
-    def create
-        @current_post = Post.find(params[:post_id])
-        @comments = @current_post.comments.new(comment_params)
-        @comments.post_id = @current_post.id
-        @comments.author_id = current_user.id
-    
-        if @comments.save
-          redirect_to user_post_path(current_user,  @current_post)
-        else
-          render :new
-        end
-      end
-    
-      private
-    
-      def comment_params
-        params.require(:comment).permit(:text)
-      end
+  def create
+    @current_post = Post.find(params[:post_id])
+    @comments = @current_post.comments.new(comment_params)
+    @comments.post_id = @current_post.id
+    @comments.author_id = current_user.id
+
+    if @comments.save
+      redirect_to user_post_path(current_user, @current_post)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
 end
