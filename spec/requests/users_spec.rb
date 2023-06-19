5791a5d3a038d8a379796ec6e266c3ed39f2839b
isaacg <iwanyoike65@gmail.com>
2023-06-19 13:03:14 +0300

    Fix tests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,21 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
+  let(:user) { User.create!(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.', post_counter: 0) }
   describe 'GET /index' do
     it 'returns http success' do
       get '/users'
       expect(response.status).to eq(200)
       expect(response).to render_template('index')
-      expect(response.body).to include('List all users')
+      expect(response.body).to include('Users')
     end
   end
 
   describe 'GET /show' do
     it 'returns http success' do
-      get '/users/1'
+      get "/users/#{user.id}"
       expect(response.status).to eq(200)
       expect(response).to render_template('show')
-      expect(response.body).to include('List a given user')
+      expect(response.body).to include('Teacher from Poland')
     end
   end
 end
